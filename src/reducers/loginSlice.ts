1411b9bb18e33acd68051fedb19e728ebd239feb
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2026-04-23 12:43:51 +0900

    feat: setToken / clearAuth アクションを追加し setLoginStatus を削除 (#440)

--- src/reducers/loginSlice.ts
+++ src/reducers/loginSlice.ts
@@ -1,19 +1,23 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { initialLoginState} from "defaults/userDefaults";
+import { initialLoginState } from "defaults/userDefaults";
 import { User } from "types/users/session";
 
 const loginSlice = createSlice({
   name: 'login',
   initialState: initialLoginState,
   reducers: {
-    setLoginStatus(state, action: PayloadAction<boolean>) {
-      state.loginStatus = action.payload;
-    },
     setCurrentUser(state, action: PayloadAction<User>) {
       state.currentUser = action.payload;
+    },
+    setToken(state, action: PayloadAction<string>) {
+      state.token = action.payload;
+    },
+    clearAuth(state) {
+      state.currentUser = initialLoginState.currentUser;
+      state.token = '';
     }
   }
 });
 
-export const { setLoginStatus, setCurrentUser} = loginSlice.actions;
+export const { setCurrentUser, setToken, clearAuth } = loginSlice.actions;
 export default loginSlice.reducer;
